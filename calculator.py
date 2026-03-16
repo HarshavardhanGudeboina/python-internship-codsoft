7dee526d70bbb7f1f4a5bb88ed4af97db019a94a
Harshavardhan <harshavardhanharsha37@gmail.com>
2026-03-16 09:43:51 +0530

    Added calculator program

---
+++ calculator.py
@@ -0,0 +1,21 @@
+operator = input("Enter an operator(+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print(f"{operator} is not a valid operator")
